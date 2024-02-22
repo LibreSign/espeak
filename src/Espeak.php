bdcae3b6f991e990d9cbae42ba1a2592e264f7e8
Any97Cris <criscianysilva1997@gmail.com>
2024-02-22 11:04:15 -0400

    implement methods to make espeak to speak
    Signed-off-by: Crisciany Silva <crisciany.souza@librecode.coop>
    Signed-off-by: Any97Cris <criscianysilva1997@gmail.com>
    Signed-off-by: Any97Cris <crisciany.souza@librecode.coop>

--- src/Espeak.php
+++ src/Espeak.php
@@ -2,12 +2,41 @@
 
 namespace Libresign\Espeak;
 
+use InvalidArgumentException;
+
 class Espeak
 {
+    private InputOptions $inputOptions;
+    public function __construct()
+    {
+        $this->inputOptions = new InputOptions();
+    }
+
     public function getVersion()
     {
-        \exec('espeak-ng --version', $output);
-        preg_match('/: (?<version>[\d\.]+) /', $output[0], $matches);
+        $this->setOption('version');
+        $output = $this->execute();
+        preg_match('/: (?<version>[\d\.]+) /', $output, $matches);
         return $matches['version'];
+    }
+
+    public function setOption(string $name, ?string $value = null): self
+    {
+        $this->inputOptions->setOption($name, $value);
+        return $this;
+    }
+
+    public function execute(string $argument = ''): string
+    {
+        $options = ' ' . $this->inputOptions;
+        if (strlen($options) === 1) {
+            throw new InvalidArgumentException();
+        }
+        $cmd = 'espeak-ng' . $options;
+        if (strlen($argument) > 0) {
+            $cmd .= ' ' . $argument;
+        }
+        $output = \shell_exec($cmd);
+        return $output;
     }
 }
--- tests/Unit/EspeakTest.php
+++ tests/Unit/EspeakTest.php
@@ -2,6 +2,7 @@
 
 namespace Libresign\Test;
 
+use InvalidArgumentException;
 use Libresign\Espeak\Espeak;
 use PHPUnit\Framework\TestCase;
 
@@ -11,6 +12,28 @@ class EspeakTest extends TestCase
     {
         $Espeak = new Espeak();
         $actual = $Espeak->getVersion();
+        $this->assertNotEmpty($actual);
+    }
+
+    public function testGetVersionUsingOption()
+    {
+        $actual = (new Espeak())
+            ->setOption('version')
+            ->execute();
+        $this->assertMatchesRegularExpression('/: (?<version>[\d\.]+) /', $actual);
+    }
+
+    public function testThrowExceptionWhenOptionIsEmpty()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        (new Espeak())->execute();
+    }
+
+    public function testSpeakWithSuccess()
+    {
+        $actual = (new Espeak())
+            ->setOption('stdout')
+            ->execute('1');
         $this->assertNotEmpty($actual);
     }
 }
